9b2cb64fae5cea63a12d3a0f443c95a4f13712c5
Tom Chauvel <tom.chauvel@hotmail.com>
2023-04-08 17:33:27 +0200

    premier jet

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<style>
+    @font-face {
+        font-family: Rubik ;
+        src: url(Rubik-VariableFont_wght.ttf);
+    }
+    html {
+        background-color: rgb(24, 29, 35);
+        color:white;
+        font-family: Rubik;
+    }
+    
+    body {
+        padding:3em;
+    }
+
+    navbar{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
+
+<body>
+    <navbar>
+        <div><a><h1>Critikult</h1></a></div>
+        <div>
+            <a>Search</a>
+            <a>Profil</a>
+        </div>
+    </navbar>
+    <main>
+    
+        <div class="container">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>
+
+    </main>
+</body>
+
+
+</html>
